a6c1b12d28d31747456c426a20f9e6e57774e47b
Alexander Ela <ela.alexander@gmail.com>
2019-02-18 15:52:59 -0700

    Add 404 routes to app-routing-module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { SkillsComponent } from './skills/skills.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ErrorComponent } from './error/error.component';
 
 const routes: Routes = [
 	{ path: '', redirectTo: '/about', pathMatch: 'full' },
 	{ path: 'about', component: AboutComponent },
 	{ path: 'skills', component: SkillsComponent },
 	{ path: 'portfolio', component: PortfolioComponent },
+	{ path: '404', component: ErrorComponent },
+	{ path: '**', redirectTo: '/404' }
 ]
 
 @NgModule({
