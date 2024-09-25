9deec1db627d623b5e9b8dcbef853d8815f784f3
alexanderela <ela.alexander@gmail.com>
2024-09-25 15:03:34 -0600

    Reduce site to landing page.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AboutComponent } from './about/about.component';
-import { SkillsComponent } from './skills/skills.component';
-import { PortfolioComponent } from './portfolio/portfolio.component';
 import { ErrorComponent } from './error/error.component';
 import { LandingComponent } from './landing/landing.component';
 
 const routes: Routes = [
 	{ path: '', component: LandingComponent, outlet: 'landing' },
-	{ path: 'about', component: AboutComponent },
-	{ path: 'skills', component: SkillsComponent },
-	{ path: 'portfolio', component: PortfolioComponent },
 	{ path: '**', component: ErrorComponent }
 ]
 
